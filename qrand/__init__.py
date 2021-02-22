84fc5e17a2791ec835c897d72f082009d867ea26
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2021-02-22 16:12:41 -0600

    bump: version 0.1.0a0 → 0.1.0

--- qrand/__init__.py
+++ qrand/__init__.py
@@ -20,6 +20,6 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
-__version__ = "0.1.0a0"
+__version__ = "0.1.0"
 
 from ._qiskit_bitgenerator import QiskitBitGenerator
