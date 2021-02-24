a41d86dd8ba4b9826eae3374c7bde5f0052e0122
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2021-02-24 10:08:06 -0600

    bump: version 0.2.0a0 → 0.2.0b0

--- qrand/__init__.py
+++ qrand/__init__.py
@@ -23,7 +23,7 @@
 __author__ = "Pedro Rivero"
 __copyright__ = "Copyright (c) 2021 Pedro Rivero"
 __license__ = "Apache-2.0"
-__version__ = "0.2.0a0"
+__version__ = "0.2.0b0"
 
 from ._qiskit_bit_generator import QiskitBitGenerator
 from ._qrng import Qrng
