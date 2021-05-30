1065968889a98995a7698c2ae8ca47711d6056e8
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2021-05-30 11:13:57 -0500

    docs(validation): add docstring

--- qrand/validation/monobit_frequency.py
+++ qrand/validation/monobit_frequency.py
@@ -1,3 +1,25 @@
+##    _____  _____
+##   |  __ \|  __ \    AUTHOR: Vishnu Ajith, Pedro Rivero
+##   | |__) | |__) |   ---------------------------------
+##   |  ___/|  _  /    DATE: May 30, 2021
+##   | |    | | \ \    ---------------------------------
+##   |_|    |_|  \_\   https://github.com/pedrorrivero
+##
+
+## Copyright 2021 Pedro Rivero
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
 from math import erfc, sqrt
 
 from . import ValidationStrategy
@@ -5,30 +27,25 @@ from . import ValidationStrategy
 
 class MonobitFrequencyValidation(ValidationStrategy):
     """
-    Frequency tests the bitstring to ensure randomness
+    Frequency (Monobit) Test: pg. 2-2 in [1]_.
 
     Methods
-    ----------
+    -------
     validate(bitstring:str) -> bool
-        validates the bitsting according to the Frequency test[monobit] defined in
-        'A Statistical Test Suite for Random and Pseudorandom Number Generators for Cryptographic Applications' <https://csrc.nist.gov/publications/detail/sp/800-22/rev-1a/final>
-        pg 24
+        Validates the randomness/entropy in an input bitstring.
 
+    References
+    ----------
+    .. [1] Lawrence Bassham, Andrew Rukhin, Juan Soto, James Nechvatal, Miles
+        Smid, Elaine Barker, Stefan Leigh, Mark Levenson, Mark Vangel, David
+        Banks, N. Heckert, James Dray (2010) A Statistical Test Suite for
+        Random and Pseudorandom Number Generators for Cryptographic
+        Applications. (U.S. Department of Commerce, Washington, D.C.), Special
+        Publication (SP) 800-22, Rev. 1a, Final April 30, 2010.
+        https://doi.org/10.6028/NIST.SP.800-22r1a
     """
 
     def validate(self, bitstring: str) -> bool:
-        """
-        PARAMETERS
-        ----------
-        bitstring: str
-        The bitstring to be tested for randomness
-
-        RETURNS
-        -------
-        out: bool
-        `True` if succeeds, `False` otherwise.
-
-        """
         n = len(bitstring)
         s_n = 0.0
         for bit in bitstring:
